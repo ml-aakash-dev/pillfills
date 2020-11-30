ae1f305ded6f2f45f1133eaff3c8c50f75eb82f6
ml-aakash-dev <ml.aakash.dev@gmail.com>
2020-12-01 04:09:44 +0500

    first commit

---
+++ src/components/Body.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+import Header from './header/Header'
+
+class Body extends Component {
+    render() {
+        return (
+            <div className="body">
+                <Header />
+            </div>
+        )
+    }
+}
+
+export default Body
---
+++ src/components/header/Header.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+import '../../css/header/header.css'
+
+class Header extends Component {
+    render() {
+        return (
+            <div className="header">
+                <div className="bgimg">
+                    <div className="wrapper">
+
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Header
